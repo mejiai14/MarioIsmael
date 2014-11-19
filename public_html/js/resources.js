4f9caed68c8a37f54a6a14bd7a75e3cab7f748c7
ismael mejia <zombi3slaya@live.com>
2014-11-19 14:46:27 -0800

    first

---
+++ public_html/js/resources.js
@@ -0,0 +1,28 @@
+game.resources = [
+
+	/* Graphics. 
+	 * @example
+	 * {name: "example", type:"image", src: "data/img/example.png"},
+	 */
+        {name: "background-tiles", type:"image", src: "data/img/backgroung-tiles.png"},
+	/* Atlases 
+	 * @example
+	 * {name: "example_tps", type: "tps", src: "data/img/example_tps.json"},
+	 */
+		
+	/* Maps. 
+	 * @example
+	 * {name: "example01", type: "tmx", src: "data/map/example01.tmx"},
+	 * {name: "example01", type: "tmx", src: "data/map/example01.json"},
+ 	 */
+           {name: "Ismael-mapLevel01", type: "tmx", src: "data/map/Ismael-mapLevel01.tmx"}
+	/* Background music. 
+	 * @example
+	 * {name: "example_bgm", type: "audio", src: "data/bgm/"},
+	 */	
+
+	/* Sound effects. 
+	 * @example
+	 * {name: "example_sfx", type: "audio", src: "data/sfx/"}
+	 */
+];
