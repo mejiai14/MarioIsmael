610e952df7e7266227221440242437d32b91573a
ismael mejia <zombi3slaya@live.com>
2014-11-19 21:16:23 -0800

    fin vid 9

--- public_html/js/entities/entities.js
+++ public_html/js/entities/entities.js
@@ -11,16 +11,32 @@ game.PlayerEntity = me.Entity.extend({
                     return (new me.rect(0, 0, 128, 128)).toPolygon();
                 }
         }]);
+    
+    this.renderable.addAnimation("idle", [3]);
+    this.renderable.addAnimation("smallWalk", [8, 9, 10, 11, 12, 13], 80);
+    
+    this.renderable.setCurrentAnimation("idle");
         
         this.body.setVelocity(5, 20);
 },
     update: function(delta){
         if(me.input.isKeyPressed("right")){
             this.body.vel.x += this.body.accel.x * me.timer.tick;
+           
         }else{
             this.body.vel.x = 0;
         }
+        if(this.body.vel.x !== 0) {
+            if(!this.renderable.isCurrentAnimation("smallWalk"))
+             this.reenderable.setCurrentAnimation("smallWalk");
+         this.renderable.setAnimationFrame();
+        }else{
+            this.renderable.setCurrentAnimation("idle");
+        }
+        
+        
         this.body.update(delta);
+        this._super(me.entity, "update", [delta]);
         return true;
     }
     
