f760cbec517c0e10439de34647a01370ea92526d
ismael mejia <zombi3slaya@live.com>
2014-11-21 15:00:41 -0800

    did more than 10 mins of work

--- public_html/js/screens/title.js
+++ public_html/js/screens/title.js
@@ -3,8 +3,28 @@ game.TitleScreen = me.ScreenObject.extend({
 	 *  action to perform on state change
 	 */
 	onResetEvent: function() {	
-		var titleImage = new me.sprite(0, 0, me.loader.getImage("title-screen"));
+		var titleImage = ( new me.sprite(0, 0, me.loader.getImage("title-screen")), 3);
                 me.game.world.addChild(titleImage, 1);
+                
+                me.input.bindKey(me.input.KEY.ENTER, "start");
+                
+                me.game.world.addChild(new (me.Renderable.extend ({
+                    init: function () {
+                        this._super(me.Renderable, 'init', [510, 30, me.game.viewport.width, me.game.viewport.height]);
+                        
+                    },
+                    
+                    draw: function () {
+                        
+                    }
+                    
+                })));
+                
+                this.handler = me.event.subscribe(me.event.KEYDOWN, function(action, keyCode, edge){
+                    if(action === "start"){
+                        me.state.change(me.state.PLAY);
+                    }
+                });
 	},
 	
 	
@@ -12,6 +32,6 @@ game.TitleScreen = me.ScreenObject.extend({
 	 *  action to perform when leaving this screen (state change)
 	 */
 	onDestroyEvent: function() {
-		; // TODO
+		me.input.unbindKey(me.input.KEY.ENTER);
 	}
 });
