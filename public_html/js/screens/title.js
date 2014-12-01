d1df5d3e6031733378e3924d4c0b62bea4c6c412
ismael mejia <zombi3slaya@live.com>
2014-12-01 14:54:29 -0800

    Fin vid 16

--- public_html/js/screens/title.js
+++ public_html/js/screens/title.js
@@ -3,7 +3,7 @@ game.TitleScreen = me.ScreenObject.extend({
 	 *  action to perform on state change
 	 */
 	onResetEvent: function() {	
-		var titleImage = ( new me.sprite(0, 0, me.loader.getImage("title-screen")), 3);
+		var titleImage = ( new me.sprite(0, 0, me.loader.getImage("title-screen")), -10);
                 me.game.world.addChild(titleImage, 1);
                 
                 me.input.bindKey(me.input.KEY.ENTER, "start");
@@ -11,11 +11,13 @@ game.TitleScreen = me.ScreenObject.extend({
                 me.game.world.addChild(new (me.Renderable.extend ({
                     init: function () {
                         this._super(me.Renderable, 'init', [510, 30, me.game.viewport.width, me.game.viewport.height]);
+                        this.font = new me.Font("Arial", 46, "white");
                         
                     },
                     
-                    draw: function () {
-                        
+                    draw: function (renderer) {
+                        this.font.draw(renderer.getContext(), "Mario", 450, 130);
+                        this.font.draw(renderer.getContext(), "Press ENTER to play!", 250, 530);
                     }
                     
                 })));
